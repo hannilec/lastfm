f45c0ab1e4a2d60ff1af531d0c0525604468e7d1
wrozka <pawelpierzchala@gmail.com>
2010-04-26 14:08:42 +0200

    pobieranie trackow kiwska

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -7,7 +7,9 @@ package crawler;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import net.roarsoftware.lastfm.Session;
 import net.roarsoftware.lastfm.User;
 
@@ -17,9 +19,12 @@ import net.roarsoftware.lastfm.User;
  */
 public class Crawler {
     private static String key = "0d64b5ba5584e52f89f0648f1b384bff";
-
+   
     public static void main(String[] args){
-        addUserFriends("mrocznaowca");
+        String name = "kiwiora";
+
+            addUserFriends(name);
+        
     }
 
     public static void addUserFriends(String name) {
@@ -28,6 +33,7 @@ public class Crawler {
         
         List l = new ArrayList();
         for(User usr : friends) {
+            
             hiberex.User friend = hiberex.User.getUserByName(usr.getName());
             friend.setName(usr.getName());
             friend.setAge(usr.getAge());
@@ -35,10 +41,13 @@ public class Crawler {
             if (usr.getGender() != null)
                 friend.setGender(usr.getGender().charAt(0));
 
+            System.out.println(usr.getName() + " " + usr.getCountry() + " " + usr.getGender());
+
             l.add(friend);
         }
 
         user.setFriends(l);
+     
     }
 
 }
