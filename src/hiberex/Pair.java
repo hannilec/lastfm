cf22c3e1725d33ca74e240baa23948625a288622
hannilec <justyna.plewa@gmail.com>
2010-05-30 13:39:26 +0200

    Pair

---
+++ src/hiberex/Pair.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hiberex;
+
+/**
+ *
+ * @author robak
+ */
+public class Pair {
+
+            public int trid;
+            public int nr;
+            public Pair(int tr,int nr){
+                this.trid=tr;
+                this.nr=nr;
+            }
+        
+}
